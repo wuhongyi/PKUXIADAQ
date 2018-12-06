949f8c974eae0e22a6609b6ba8d580116456183a
wuhongyi <wuhongyi@qq.com>
2018-12-06 22:56:25 +0800

    20181206

---
+++ HPGeBGO/tran.cc
@@ -0,0 +1,24 @@
+// tran.cc --- 
+// 
+// Description: 
+// Author: Hongyi Wu(吴鸿毅)
+// Email: wuhongyi@qq.com 
+// Created: 四 12月  6 22:48:21 2018 (+0800)
+// Last-Updated: 四 12月  6 22:48:48 2018 (+0800)
+//           By: Hongyi Wu(吴鸿毅)
+//     Update #: 1
+// URL: http://wuhongyi.cn 
+
+#include "wuReadData.hh"
+
+int main(int argc, char *argv[])
+{
+
+
+
+  
+  return 0;
+}
+
+// 
+// tran.cc ends here
